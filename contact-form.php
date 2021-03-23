3e9d1ed0b3c1690e65027684925db4f58599d1da
chrislabours <chrislabours@gmail.com>
2021-03-23 14:16:46 +0800

    Add contact-form.php

---
+++ contact-form.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $mailTo = "mail@mail.com";
+    $headers = "From: ".$email;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
+
+?>
